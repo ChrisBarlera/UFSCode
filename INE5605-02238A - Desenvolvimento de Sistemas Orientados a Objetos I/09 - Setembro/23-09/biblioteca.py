a26c8c101defcf99006e4546c77366e30943323b
Christian Barlera <christian.barlera2@gmail.com>
2024-09-27 18:50:10 -0300

    Iniciando Exercício 4

---
+++ INE5605-02238A - Desenvolvimento de Sistemas Orientados a Objetos I/09 - Setembro/23-09/biblioteca.py
@@ -0,0 +1,12 @@
+class Biblioteca:
+
+    def __init__(self):
+        pass
+
+    # @property
+    # def nome(self):
+    #     return self.__nome
+
+    # @nome.setter
+    # def nome(self, nome: str):
+    #     self.__nome = nome
---
+++ INE5605-02238A - Desenvolvimento de Sistemas Orientados a Objetos I/09 - Setembro/23-09/livro.py
@@ -0,0 +1,71 @@
+from editora import Editora
+from autor import Autor
+
+class Livro:
+
+    def __init__(self, codigo: int, titulo: str,
+                 ano: int, editora: Editora, autor: Autor,
+                 numero_capitulo: int, titulo_capitulo: str):
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__ano = ano
+        self.__editora = editora
+        self.__autor = autor
+        self.__numero_capitulo = numero_capitulo
+        self.__titulo_capitulo = titulo_capitulo
+
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @codigo.setter
+    def codigo(self, codigo: int):
+        self.__codigo = codigo
+
+    @property
+    def titulo(self):
+        return self.__titulo
+
+    @titulo.setter
+    def titulo(self, titulo: str):
+        self.__titulo = titulo
+
+    @property
+    def ano(self):
+        return self.__ano
+
+    @ano.setter
+    def ano(self, ano: int):
+        self.__ano = ano
+
+    @property
+    def editora(self):
+        return self.__editora
+
+    @editora.setter
+    def editora(self, editora: Editora):
+        self.__editora = editora
+    
+    @property
+    def autor(self):
+        return self.__autor
+
+    @autor.setter
+    def autor(self, autor: Autor):
+        self.__autor = autor
+
+    @property
+    def numero_capitulo(self):
+        return self.__numero_capitulo
+
+    @numero_capitulo.setter
+    def numero_capitulo(self, numero_capitulo: int):
+        self.__numero_capitulo = numero_capitulo
+    
+    @property
+    def titulo_capitulo(self):
+        return self.__titulo_capitulo
+
+    @titulo_capitulo.setter
+    def titulo_capitulo(self, titulo_capitulo):
+        self.__titulo_capitulo = titulo_capitulo
