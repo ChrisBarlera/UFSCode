aac813a5157a4e0114fdf0a096d76f9b70ceabb7
Christian Barlera <christian.barlera2@gmail.com>
2024-10-14 13:48:09 -0300

    Reorganizando arquivos

--- INE5605-02238A - Desenvolvimento de Sistemas Orientados a Objetos I/09 - Setembro/30-09/mainteste.py
+++ INE5605-02238A - Desenvolvimento de Sistemas Orientados a Objetos I/09 - Setembro/30-09/mainteste.py
@@ -1,5 +1,59 @@
-from aluno_posgraduacao import AlunoPosGraduacao
+from controladorChamados import ControladorChamados
+from controladorPessoas import ControladorPessoas
+from chamado import Chamado
+from cliente import Cliente
+from tecnico import Tecnico
+from tipoChamado import TipoChamado
+from datetime import date as Date
 
-posgrad = AlunoPosGraduacao(109,12,23101)
+datateste = Date(2024,12,21)
+clienteteste = Cliente('Fulano', 123)
+tecnicoteste = Tecnico('Técnico', 321)
+tipoteste = TipoChamado(1,'Um tipo de chamado', 'Tipo 1')
 
-posgrad.emprestar('Cronicas')
+controlador_chamados = ControladorChamados()
+controlador_chamados.inclui_chamado(datateste,
+                                    clienteteste,
+                                    tecnicoteste,
+                                    'pane no sistema',
+                                    'alguém me desconfigurou',
+                                    2,
+                                    tipoteste)
+
+controlador_chamados.inclui_chamado(datateste,
+                                    clienteteste,
+                                    tecnicoteste,
+                                    'pane no sistema',
+                                    'alguém me desconfigurou',
+                                    2,
+                                    tipoteste)
+
+controlador_chamados.inclui_tipochamado(1,
+                                        'Pane',
+                                        'Tem pane no sistema')
+controlador_chamados.inclui_tipochamado(1,
+                                        'Pane',
+                                        'Tem pane no sistema')
+
+print(controlador_chamados.chamados)
+print(controlador_chamados.tipos_chamados)
+
+tc = TipoChamado(1,1,"Bug")
+print(tc.codigo,tc.descricao,tc.nome) # 1 1 Bug
+c = Cliente("Cliente", 2)
+print(c.nome,c.codigo) # Cliente 2
+t = Tecnico("Tecnico", 2)
+print(t.nome,t.codigo) # Tecnico 2
+ch = Chamado(Date.today(), c, t, "titulo", "descricao", 4, tc)
+print(ch.tipo.nome) # Bug
+
+cp = ControladorPessoas()
+cp.inclui_cliente(1, "cli")
+cp.inclui_tecnico(1, "tec")
+for c in cp.clientes: # cli
+    print(c.nome)
+for t in cp.tecnicos: # tec
+    print(t.nome)
+
+cc = ControladorChamados()
+print(cc.total_chamados_por_tipo(tc)) # 0
---
+++ INE5605-02238A - Desenvolvimento de Sistemas Orientados a Objetos I/10 - Outubro/07-10/Personagem.py
@@ -0,0 +1,34 @@
+from AbstractPersonagem import *
+
+class Tipo(Enum):
+
+    ar = "ar"
+    terra = "terra"
+    fogo = "fogo"
+    agua = "agua"
+
+class Personagem(AbstractPersonagem):
+    #Construtor fornecido, nao deve ser alterado
+    def __init__(self, energia: int, habilidade: int,
+                 velocidade: int, resistencia: int, tipo: Tipo):
+        pass#implementar
+
+    @property
+    def tipo(self) -> Tipo:
+        pass#implementar
+
+    @property
+    def energia(self) -> int:
+        pass#implementar
+
+    @property
+    def habilidade(self) -> int:
+        pass#implementar
+
+    @property
+    def velocidade(self) -> int:
+        pass#implementar
+
+    @property
+    def resistencia(self) -> int:
+        pass#implementar
